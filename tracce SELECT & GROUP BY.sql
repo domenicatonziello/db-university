5ff459a5a9b0b2d34fb57425e1543629aa2aead0
domenicatonziello <domenica.tonziello10@gmail.com>
2023-02-14 16:33:37 +0100

    Group By esercizi

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -45,12 +45,21 @@ WHERE `phone` IS NULL;
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-
+SELECT COUNT(*) AS 'iscritti', YEAR(`enrolment_date`) AS 'anno'  
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-
+SELECT COUNT(*) AS 'insegnanti', `office_address` 
+FROM `teachers`
+GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT `student_id`, AVG(`vote`) AS 'media'     
+FROM `exam_student`
+GROUP BY `student_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT COUNT(`name`) AS 'Totale corsi', `department_id`   
+FROM `degrees`
+GROUP BY `department_id`;
